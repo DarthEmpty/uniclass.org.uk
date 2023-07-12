0f782ff0082606910d4837deece07b506a17318a
Joel Clarke <joel.clarke273@gmail.com>
2023-07-12 15:59:07 +0100

    initial commit: begin work on landing page

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+<section class="bg-white dark:bg-gray-900">
+  <!-- Navbar -->
+
+<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="/" class="mx-auto">
+      <img src="./assets/uniclass.jpeg" class="h-8 mr-3" alt="Flowbite Logo"/>
+  </a>
+  
+  </div>
+</nav>
+
+
+  <!-- Avatar -->
+  <img class="mx-auto rounded-full" src="./assets/ed.jpeg"/>
+
+  <!-- Jumbotron -->
+  <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Ed Freeman</h1>
+    <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et velit consequat, suscipit tellus a, ornare dolor. Aliquam erat volutpat. Morbi dignissim elementum scelerisque. Fusce hendrerit ante nibh, nec commodo. </p>
+  </div>
+</section>
+
+</template>
+
+<script setup lang="ts">
+</script>
+
